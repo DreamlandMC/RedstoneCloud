9b6ef7386ecca4730efa68e80e35c80143a920db
tunikakeks <keksdev9513@gmail.com>
2024-12-28 16:40:42 +0100

    Command to kill server

---
+++ cloud/src/main/java/de/redstonecloud/cloud/commands/defaults/KillCommand.java
@@ -0,0 +1,51 @@
+package de.redstonecloud.cloud.commands.defaults;
+
+import de.redstonecloud.cloud.RedstoneCloud;
+import de.redstonecloud.cloud.commands.Command;
+import de.redstonecloud.cloud.logger.Logger;
+import de.redstonecloud.cloud.server.Server;
+import io.netty.util.internal.EmptyArrays;
+
+import java.util.Arrays;
+
+public class KillCommand extends Command {
+    public int argCount = 1;
+
+    public KillCommand(String cmd) {
+        super(cmd);
+    }
+
+    @Override
+    protected void onCommand(String[] args) {
+        if (args.length == 0) {
+            Logger.getInstance().error("Usage: kill <server>");
+            return;
+        }
+
+        Server server = RedstoneCloud.getInstance().getServerManager().getServer(args[0]);
+        if (server == null) {
+            if (args[0].endsWith("*")) {
+                Server[] servers = RedstoneCloud.getInstance().getServerManager().getServers().values().toArray(new Server[0]);
+                Server[] affectedServers = Arrays.stream(servers).filter(s -> s.getName().startsWith(args[0].substring(0, args[0].length() - 1))).toArray(Server[]::new);
+
+                for (Server s : affectedServers) {
+                    s.kill();
+                }
+
+                Logger.getInstance().info("Stopped " + affectedServers.length + " servers");
+                return;
+            }
+
+
+            Logger.getInstance().error("Server not found.");
+            return;
+        }
+
+        server.kill();
+    }
+
+    @Override
+    public String[] getArgs() {
+        return getServer().getServerManager().getServers().keySet().toArray(EmptyArrays.EMPTY_STRINGS);
+    }
+}
